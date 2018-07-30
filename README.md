d6cd968d2f623c3c98882333f8e80127dc3d8324
Paul Soucy <paul@dev-smart.com>
2018-07-29 22:49:51 -0400

    it works

---
+++ README.md
@@ -0,0 +1,3 @@
+### Project FileTree ###
+
+This project uses FUSE to mount a filesystem backed by a de-duplicating storage system
---
+++ src/main/cpp/pch.h
@@ -0,0 +1,16 @@
+#ifndef PCH_H
+#define PCH_H
+
+#define FUSE_USE_VERSION 30
+#include <fuse.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <iostream>
+
+
+#endif // PCH_H
