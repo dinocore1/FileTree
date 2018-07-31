343f901f11c8921925b991d2265f7435eccd5897
Paul Soucy <paul@dev-smart.com>
2018-07-30 23:05:23 -0400

    wip

--- src/main/cpp/datafile.h
+++ src/main/cpp/datafile.h
@@ -1,6 +1,17 @@
 #ifndef DATAFILE_H
 #define DATAFILE_H
 
+typedef uint32_t blockptr_t;
+
+struct Block {
+    uint8_t data[4096];
+};
+
+struct INode {
+    uint32_t size;
+    uint16_t link_count;
+    blockptr_t block[60];
+};
 
 class DataFile {
 public:
